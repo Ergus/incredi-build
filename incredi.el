83d74183892a4769f69dc7dc42065800d38e6cbf
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2023-04-07 13:29:35 +0200

    Some fixes getting the files to save and the list of completions

--- incredi.el
+++ incredi.el
@@ -32,14 +32,17 @@
   :group 'tools
   :group 'processes)
 
-(defconst incredi-re "^Project(\"{\\([A-Z0-9\-]+\\)}\") = \"\\([^\"]+\\)\", \"\\([^\"]+\\)\", \"{\\([A-Z0-9\-]+\\)}\"$"
+(defcustom incredi-executable (executable-find "BuildConsole")
+  "Incredibuild executable")
+
+(defconst incredi-regex "^Project(\"{\\([A-Z0-9\-]+\\)}\") = \"\\([^\"]+\\)\", \"\\([^\"]+\\)\", \"{\\([A-Z0-9\-]+\\)}\"$"
   "Regular expression to search.")
 
 (defvar incredi--tree nil "Remember the vs incredibuild tree.")
 
 (defun incredi--get-sln (&optional dir)
   "Return the .sln file in DIR if it exists or nil otherwise."
-  (car (directory-files (or dir default-directory) nil "\\.sln$")))
+  (car (directory-files (or dir incredi-dir) nil "\\.sln$")))
 
 (defun incredi--get-tree (dir regex)
   "Get a list of DIR's dominant directories with a file with a REGEX name."
@@ -63,7 +66,7 @@
 	      (out (make-hash-table)))
     (with-temp-buffer
       (insert-file-contents file)
-      (while (re-search-forward re-project nil t)
+      (while (re-search-forward incredi-regex nil t)
 	(puthash (match-string-no-properties 2)
 		 (list :path (match-string-no-properties 3)
 		       :guid (match-string-no-properties 4))
@@ -75,6 +78,15 @@
 
 (defvar-local incredi-last-build-dir nil)
 (defvar-local incredi-last-build-project nil)
+(defvar-local incredi-last-compile-command nil)
+
+(defvar incredi-dir nil
+  "Global value of current build directory.")
+
+(defun incredi--buffer-file-in-project ()
+  (and buffer-file-name
+       (string-prefix-p incredi-dir
+			(expand-file-name buffer-file-name))))
 
 ;;;###autoload
 (defun incredi-build (arg)
@@ -82,19 +94,30 @@
   (declare (interactive-only compile))
   (interactive "P")
   ;; Clean the cached variables
-  (when arg
-    (setq incredi-last-build-dir nil))
-  (let* ((default-directory (or incredi-last-build-dir
-				(setq incredi-last-build-project nil) ;; clean this one if I will read a new project.
-				(completing-read "Directory: " incredi-tree)))
-	 (project (or incredi-last-build-project
-		      (completing-read "Project: "
-				       (or (and (not arg)
-						(assoc default-directory incredi-projects-alist #'string-equal))
-					   (setf (alist-get default-directory incredi-projects-alist nil 'remove #'string-equal)
-						 (incredi--parse-sln (incredi--get-sln))))))))
-    (setq incredi-last-build-dir default-directory
-	  incredi-last-build-project project)
-    (call-interactively #'compile)))
+  (setq incredi-last-build-project nil) ;; clean this one if I will read a new project.
+  (let* ((incredi-dir (completing-read "Directory: " #'incredi-tree nil incredi-last-build-dir))
+	 (default-directory incredi-dir) ;; This here not befor not after
+	 (project (completing-read "Project: "
+				   (or (alist-get incredi-dir incredi-projects-alist nil nil #'string-equal)
+				       (cdar (push (cons incredi-dir (incredi--parse-sln (incredi--get-sln incredi-dir)))
+						   incredi-projects-alist)))
+				   nil incredi-last-build-project))
+	 (command (or incredi-last-compile-command
+		      (format "%s %s /build /prj=%s /cfg=\"Debug|Win32\""
+			      (shell-quote-argument incredi-executable)
+			      (incredi--get-sln incredi-dir)
+			      project))))
+
+    (save-some-buffers (not compilation-ask-about-save)
+                       #'incredi--buffer-file-in-project)
+
+    (setq-local incredi-last-build-dir incredi-dir
+		incredi-last-build-project project
+		incredi-last-compile-command command)
+
+    (setq-default compilation-directory incredi-dir
+		  compile-command command)
+    (message "command: %s" command)
+    (compilation-start command)))
 
 (provide 'incredi)
