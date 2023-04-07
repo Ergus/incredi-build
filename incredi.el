6128f342fd4cfbcebc40d99118e636015685617d
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2023-04-07 13:29:35 +0200

    Some changes preparing to make build only

--- incredi.el
+++ incredi.el
@@ -32,20 +32,28 @@
   :group 'tools
   :group 'processes)
 
-(defcustom incredi-executable (executable-find "BuildConsole")
-  "Incredibuild executable")
+(defcustom incredi-incredibuild (let ((exe (executable-find "BuildConsole")))
+				(when exe (file-name-nondirectory exe)))
+  "Incredibuild executable"
+  :local t)
+
+(defcustom incredi-msbuild (let ((exe (executable-find "MSBuild")))
+			     (when exe (file-name-nondirectory exe)))
+  "Incredibuild executable"
+  :local t)
 
 (defconst incredi-regex "^Project(\"{\\([A-Z0-9\-]+\\)}\") = \"\\([^\"]+\\)\", \"\\([^\"]+\\)\", \"{\\([A-Z0-9\-]+\\)}\"$"
   "Regular expression to search.")
 
-(defvar incredi--tree nil "Remember the vs incredibuild tree.")
+(defvar-local incredi--sln-tree nil "Remember the vs incredibuild tree.")
+(defvar incredi--history nil "History of incredibuild commands.")
 
-(defun incredi--get-sln (&optional dir)
+(defun incredi--get-sln (dir)
   "Return the .sln file in DIR if it exists or nil otherwise."
-  (car (directory-files (or dir incredi-dir) nil "\\.sln$")))
+  (car (directory-files dir t "\\.sln$" t 1)))
 
 (defun incredi--get-tree (dir regex)
-  "Get a list of DIR's dominant directories with a file with a REGEX name."
+  "Get a list of DIR's dominant directories containing a file with name matching REGEX."
   (let (out)
     (while-let ((path (and dir
 			   (locate-dominating-file dir #'incredi--get-sln))))
@@ -53,12 +61,12 @@
       (setq dir (file-name-directory (directory-file-name path))))
     out))
 
-(defun incredi-tree ()
+(defun incredi--get-sln-tree (dir)
   "Return a tree of visualstrudio directories with .sln files."
-  (with-memoization incredi--tree
+  (with-memoization incredi--sln-tree
     ;; use default-directory-here, because we call this function
     ;; before setting INCREDI-DIR
-    (incredi--get-tree default-directory "\\.sln$")))
+    (incredi--get-tree dir "\\.sln$")))
 
 (defun incredi--parse-sln (file)
   "Parse the project lines in FILE and return a list of projects."
@@ -68,59 +76,92 @@
     (with-temp-buffer
       (insert-file-contents file)
       (while (re-search-forward incredi-regex nil t)
-	(puthash (match-string-no-properties 2)
-		 (list :path (match-string-no-properties 3)
-		       :guid (match-string-no-properties 4))
-		 out)))
+	(let* ((projname (match-string-no-properties 2))
+	       (path (match-string-no-properties 3))
+	       (guid (match-string-no-properties 4))
+	       (plist (gethash projname out)))
+
+	  (if (file-name-extension path)
+	      (plist-put plist :file path)
+	    (plist-put plist :dir path))
+
+	  (puthash projname plist out))))
     out))
 
+;; Project alist and cache
 (defvar incredi--projects-alist nil
   "Alist for projects with directory:hashtable" )
 
-(defun incredi-projects (&rest _)
-  "Return the list of projects in the .sln of default directory"
-  (or (alist-get incredi-dir incredi--projects-alist nil nil #'string-equal)
-      (cdar (push (cons incredi-dir (incredi--parse-sln (incredi--get-sln incredi-dir)))
-		  incredi--projects-alist))))
+(defun incredi--sln-projects (file)
+  "Return the list of projects in the .sln of dir"
+  (let ((dir (file-name-directory file)))
+    (or (alist-get dir incredi--projects-alist nil nil #'string-equal)
+	(cdar (push (cons dir (incredi--parse-sln file))
+		    incredi--projects-alist)))))
 
-(defvar-local incredi-last-build-dir nil)
-(defvar-local incredi-last-build-project nil)
+;; Info cache
+(defvar-local incredi--info nil
+  "Variable with all the last build information")
 
-(defvar incredi-dir nil
-  "Global value of current build directory.")
+(defun incredi--read-info (mode)
+  "Ask the user for the build information. And return the info list"
+  (let* ((dir (completing-read "Directory: "
+			       (incredi--get-sln-tree default-directory)
+			       nil t
+			       (plist-get incredi--info :dir)))
+	 (file (incredi--get-sln dir))
+	 (project (completing-read "Project: "
+				   (incredi--sln-projects file)
+				   nil t
+				   (and (string-equal dir (plist-get incredi--info :dir))
+					(plist-get incredi--info :project))))
+	 (exe (shell-quote-argument
+		   (pcase mode
+		     ('build incredi-incredibuild)
+		     ('only incredi-msbuild)
+		     (_ (error "Mode '%s not known" mode))))))
 
-(defun incredi--buffer-file-in-project ()
-  (and buffer-file-name
-       ;; We use INCREDI-DIR herebecause this evaluated separatelly in
-       ;; every buffer where the defaultdirectory will change.
-       (string-prefix-p incredi-dir
-			(expand-file-name buffer-file-name))))
+    (list :dir dir :project project :exe exe :sln file)))
 
-;;;###autoload
-(defun incredi-build (arg)
+
+(defun incredi--build-command (mode pinfo)
+  "Should return the build command to use.
+PINFO is used to get the build information."
+  (pcase mode
+    ('build (format "%s %s /build /prj=%s /cfg=\"Debug|Win32\""
+		    (plist-get pinfo :exe)
+		    (plist-get pinfo :sln)
+		    (plist-get pinfo :project)))
+    (_ (error "Error composing build command"))))
+
+(defun incredi--build-internal (mode)
   "Run `compile' in the project root."
-  (declare (interactive-only compile))
-  (interactive "P")
   ;; Clean the cached variables
-  (let* ((incredi-dir (completing-read "Directory: " (incredi-tree) nil incredi-last-build-dir))
-	 (default-directory incredi-dir) ;; This here not befor not after
-	 (project (completing-read "Project: " (incredi-projects) nil
-				   (when (string-equal incredi-dir incredi-last-build-dir)
-				     incredi-last-build-project)))
-	 (command (format "%s %s /build /prj=%s /cfg=\"Debug|Win32\""
-			      (shell-quote-argument incredi-executable)
-			      (incredi--get-sln incredi-dir)
-			      project)))
+  (let* ((pinfo (incredi--read-info mode))
+	 (default-directory (plist-get pinfo :dir))
+	 (command (read-shell-command "Command: "
+				      (incredi--build-command mode pinfo)
+				      'incredi--history)))
+
+    (add-to-history 'incredi--history command)
 
     (save-some-buffers (not compilation-ask-about-save)
-                       #'incredi--buffer-file-in-project)
+                       (lambda ()
+			 (and buffer-file-name
+			      (string-prefix-p (plist-get pinfo :dir)
+					       (expand-file-name buffer-file-name)))))
 
-    (setq-local incredi-last-build-dir incredi-dir
-		incredi-last-build-project project)
+    (setq-local incredi--info pinfo)  ;; This before save-some-buffers
 
-    (setq-default compilation-directory incredi-dir
-		  compile-command command)
-    (message "command: %s" command)
+    (unless (equal compile-command command)
+      (setq compile-command command))
+    (setq-default compilation-directory default-directory)
     (compilation-start command)))
+
+;;;###autoload
+(defun incredi-build ()
+  "Run `compile' in the project root."
+  (interactive)
+  (incredi--build-internal 'build))
 
 (provide 'incredi)
